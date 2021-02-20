0e8107f7098d7c556492dde5c55f5ff42245ffdf
Loudiy <loudiynessreddine@gmail.com>
2021-02-20 22:13:29 +0100

    Change script

--- WebScrapper.py
+++ WebScrapper.py
@@ -6,7 +6,7 @@ import os
 import xml.etree.ElementTree as gfg
 
 #### Define time to wait
-time_to_sleep = 10
+time_to_sleep = 5
 
 #### URLs of the website
 url_home = "https://hermanosautosales.com"
