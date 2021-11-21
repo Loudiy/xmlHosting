52b9f229604e4b40b3751c364bf77c3aeecbece4
Loudiy <loudiynessreddine@gmail.com>
2021-11-21 15:13:21 +0100

    Add command to add the date of last execution

--- push.sh
+++ push.sh
@@ -9,5 +9,8 @@ do
 	git pull
 	git add xml/xmlIndex.xml
 	git commit -m "Update catalog `date`"
+	date > LastModified.txt
+	git add LastModified.txt
+	git commit -m "Update last time modified"
 	git push
 done
