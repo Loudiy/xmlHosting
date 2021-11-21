67b11fae5e1658daf91ce8da9845242dfa85b343
Loudiy <loudiynessreddine@gmail.com>
2021-11-21 15:17:21 +0100

    Removed for loop and replaced it by infinite calling of the same file

--- push.sh
+++ push.sh
@@ -2,15 +2,13 @@
 
 cd /home/nessreddine/Desktop/Nessreddine/xmlHosting
 
-while true
-do
-	git pull
-	python3 WebScrapper.py
-	git pull
-	git add xml/xmlIndex.xml
-	git commit -m "Update catalog `date`"
-	date > LastModified.txt
-	git add LastModified.txt
-	git commit -m "Update last time modified"
-	git push
-done
+git pull
+python3 WebScrapper.py
+git pull
+git add xml/xmlIndex.xml
+git commit -m "Update catalog `date`"
+date > LastModified.txt
+git add LastModified.txt
+git commit -m "Update last time modified"
+git push
+./push.sh
