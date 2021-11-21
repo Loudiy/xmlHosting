bde404b70e87bb4d191e10e040532b8dd74eb109
Loudiy <loudiynessreddine@gmail.com>
2021-11-21 15:25:20 +0100

    Removed test of on the run modifications

--- push.sh
+++ push.sh
@@ -8,8 +8,7 @@ git pull
 git add xml/xmlIndex.xml
 git commit -m "Update catalog `date`"
 date > LastModified.txt
-"Nessredddine" > test.txt
 git add LastModified.txt
-git commit -m "Update last time modified"
+git commit -m "Update last time modified: `date`"
 git push
 ./push.sh
